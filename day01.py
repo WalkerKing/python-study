10720e283c77671d4a283a9d27bbc3a40c9e86cf
wangdaohan <wangdaohan@g7.com.cn>
2020-03-24 23:27:45 +0800

    add day01

---
+++ day01.py
@@ -0,0 +1,23 @@
+# 1
+print('==============第1题================')
+print('''
+姓名:kaikeba
+QQ:xxxxxxx
+185xxxxxxx
+公司地址:北京市xxxx
+''')
+# 2
+print('===============第2题===============')
+print('姓名:kaikeba\nQQ:xxxxxxx\n185xxxxxxx\n公司地址:北京市xxxx')
+# 3
+print('===============第3题============')
+print('答案:A')
+# 4
+print('===============第4题=============')
+print('hello，python')
+print("hello，python")
+# 5 
+print('===============第5题============')
+x = "我的身⾼是："
+y=180
+print(x + str(y))
